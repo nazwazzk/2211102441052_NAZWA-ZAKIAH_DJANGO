05bf9e1fd3600fb2cd4728773da315f822f2a9fa
nazwazzk <2211102441052@umkt.ac.id>
2025-05-23 14:44:36 +0800

    p4

---
+++ MOOSIX/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request):
+    template_name = "home.html"
+    context = {
+        'title':'MOOSIX'
+    }
+
+    return render(request, template_name, context)
+
+def about(request):
+    template_name = "about.html"
+    context = {
+        'title':'selamat datang di about'
+    }
+
+    return render(request, template_name, context)
