90bc570bf0003caa5ecf1a38f9f1f0a42d5bab84
nazwazzk <2211102441052@umkt.ac.id>
2025-05-23 15:36:45 +0800

    p5

--- MOOSIX/urls.py
+++ MOOSIX/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-from MOOSIX.views import home, about
+from django.conf import settings
+from django.conf.urls.static import static
+ 
+from .views import home, about
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', home, name="home"),   
-    path('about', about, name="about"),  
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('dashboard/', include("berita.urls")),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
---
+++ berita/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from berita.views import dashboard
+
+urlpatterns = [
+    path('', dashboard, name="dashboard")
+]
